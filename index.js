0ee7ebbc6e42221432401f6fd848bf8a09bdff95
licious <licious@LANCELOT.local>
2021-09-12 23:06:49 +0530

    Local 2 API

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -12,5 +12,5 @@ app.use(express.json());
 const PostsRouter = require('./Routes/posts');
 app.use('/posts',PostsRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port,()=> console.log('server started'));
