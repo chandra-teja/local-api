2076ae304d1093f1a48274df1c9cc0f59a84ad8b
licious <licious@LANCELOT.local>
2021-09-14 12:51:46 +0530

    Deploying

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/posts')
-.then(()=> console.log('Db connected'))
-.catch((err)=>console.log('Db connection failed',err));
+const mongoose = require("mongoose");
+require('./prod')(app);
+mongoose
+  .connect(
+    "mongodb+srv://postblog:post123@samplecluster.8quxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Db connected"))
+  .catch((err) => console.log("Db connection failed", err));
 
 app.use(express.json());
 
-const PostsRouter = require('./Routes/posts');
-app.use('/posts',PostsRouter);
+const PostsRouter = require("./Routes/posts");
+app.use("/posts", PostsRouter);
 
 const port = process.env.PORT || 5000;
-app.listen(port,()=> console.log('server started'));
+app.listen(port, () => console.log("server started"));
